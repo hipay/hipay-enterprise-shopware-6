b1a91382a658fa2435182aa14c0624f9c792895e
Adam BRECHET <abrechet@hipay.com>
2023-07-03 15:47:25 +0200

    WIp : Fix migration

--- src/Migration/Migration1688376458.php
+++ src/Migration/Migration1688376458.php
@@ -18,8 +18,19 @@ class Migration1688376458 extends MigrationStep
     {
         $sql = <<<SQL
         ALTER TABLE `hipay_order`
-            ADD `order_version_id` BINARY(16) NOT NULL,
-            ADD `order_transaction_version_id` BINARY(16) NOT NULL;
+            ADD `order_version_id` BINARY(16) UNIQUE NOT NULL,
+            ADD `transaction_version_id` BINARY(16) UNIQUE NOT NULL,
+            DROP CONSTRAINT `fk.hipay_order.order_id`,
+            DROP CONSTRAINT `fk.hipay_order.transaction_id`;
+        SQL;
+        $connection->executeStatement($sql);
+
+        $sql = <<<SQL
+        ALTER TABLE `hipay_order`
+            ADD CONSTRAINT `fk.hipay_order.order_id` FOREIGN KEY (`order_id`, `order_version_id`)
+            REFERENCES `order` (`id`, `version_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+            ADD CONSTRAINT `fk.hipay_order.transaction_id` FOREIGN KEY (`transaction_id`, `transaction_version_id`)
+            REFERENCES `order_transaction` (`id`, `version_id`) ON DELETE CASCADE ON UPDATE CASCADE;
         SQL;
         $connection->executeStatement($sql);
     }
@@ -29,7 +40,18 @@ class Migration1688376458 extends MigrationStep
         $sql = <<<SQL
         ALTER TABLE `hipay_order`
             DROP `order_version_id`,
-            DROP `order_transaction_version_id`;
+            DROP `transaction_version_id`,
+            DROP CONSTRAINT `fk.hipay_order.order_id`,
+            DROP CONSTRAINT `fk.hipay_order.transaction_id`;
+        SQL;
+        $connection->executeStatement($sql);
+
+        $sql = <<<SQL
+        ALTER TABLE `hipay_order`
+            ADD CONSTRAINT `fk.hipay_order.order_id` FOREIGN KEY (`order_id`)
+            REFERENCES `order` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+            ADD CONSTRAINT `fk.hipay_order.transaction_id` FOREIGN KEY (`transaction_id`)
+            REFERENCES `order_transaction` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
         SQL;
         $connection->executeStatement($sql);
     }
