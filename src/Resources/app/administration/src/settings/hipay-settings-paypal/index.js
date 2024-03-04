16aba80ec0c4ab0cfb5af528cb84a8b3f5cda706
Thibaut GODET <tgodet.ext@hipay.com>
2024-03-04 13:40:41 +0000

    [EC-3509][PAYPAL V2] : Ajouter PayPal v2 sur Shopware

---
+++ src/Resources/app/administration/src/settings/hipay-settings-paypal/index.js
@@ -0,0 +1,123 @@
+/**
+ * Component for hipay settings
+ * Cards selector
+ */
+import template from './hipay-settings-paypal.html.twig';
+
+Shopware.Component.register('hipay-settings-paypal', {
+  template,
+  props: {
+    isLoading: {
+      type: Boolean,
+      required: true
+    },
+    paymentMethod: {
+      type: Object,
+      required: true
+    }
+  },
+  data() {
+    return {
+      isPayPalV2: this.paymentMethod.customFields.merchantPayPalId !== "",
+      colors: [
+        {
+          label: this.$tc('hipay.settings.paypal.color.gold'),
+          value: "gold"
+        },
+        {
+          label: this.$tc('hipay.settings.paypal.color.blue'),
+          value: "blue"
+        },
+        {
+          label: this.$tc('hipay.settings.paypal.color.black'),
+          value: "black"
+        },
+        {
+          label: this.$tc('hipay.settings.paypal.color.silver'),
+          value: "silver"
+        },
+        {
+          label: this.$tc('hipay.settings.paypal.color.white'),
+          value: "white"
+        }],
+      shapes: [
+        {
+          label: this.$tc('hipay.settings.paypal.shape.pill'),
+          value: "pill"
+        },
+        {
+          label: this.$tc('hipay.settings.paypal.shape.rect'),
+          value: "rect"
+        }],
+      labels: [
+        {
+          label: this.$tc('hipay.settings.paypal.label.paypal'),
+          value: "paypal"
+        },
+        {
+          label: this.$tc('hipay.settings.paypal.label.pay'),
+          value: "pay"
+        },
+        {
+          label: this.$tc('hipay.settings.paypal.label.subscribe'),
+          value: "subscribe"
+        },
+        {
+          label: this.$tc('hipay.settings.paypal.label.checkout'),
+          value: "checkout"
+        },
+        {
+          label: this.$tc('hipay.settings.paypal.label.buynow'),
+          value: "buynow"
+        }]
+    };
+  },
+  methods: {
+    updatePayPalStatus(newValue) {
+      this.isPayPalV2 = newValue !== "" ? true : false;
+      this.updateMerchantPayPalId(newValue);
+    },
+    updateMerchantPayPalId(newValue) {
+      if (this.paymentMethod.customFields === null) {
+        this.paymentMethod.customFields = { ...this.paymentMethod.customFields, merchantPayPalId: newValue };
+      } else {
+        this.paymentMethod.customFields.merchantPayPalId = newValue;
+      }
+    },
+    updateColor(newValue) {
+      if (this.paymentMethod.customFields === null) {
+        this.paymentMethod.customFields = { ...this.paymentMethod.customFields, color: newValue };
+      } else {
+        this.paymentMethod.customFields.color = newValue;
+      }
+    },
+    updateShape(newValue) {
+      if (this.paymentMethod.customFields === null) {
+        this.paymentMethod.customFields = { ...this.paymentMethod.customFields, shape: newValue };
+      } else {
+        this.paymentMethod.customFields.shape = newValue;
+      }
+    },
+    updateLabel(newValue) {
+      if (this.paymentMethod.customFields === null) {
+        this.paymentMethod.customFields = { ...this.paymentMethod.customFields, label: newValue };
+      } else {
+        this.paymentMethod.customFields.label = newValue;
+      }
+    },
+    updateHeight(newValue) {
+      if (this.paymentMethod.customFields === null) {
+        this.paymentMethod.customFields = { ...this.paymentMethod.customFields, height: newValue };
+      } else {
+        this.paymentMethod.customFields.height = newValue;
+      }
+    },
+    updateBnpl(newValue) {
+      if (this.paymentMethod.customFields === null) {
+        this.paymentMethod.customFields = { ...this.paymentMethod.customFields, bnpl: newValue };
+      } else {
+        this.paymentMethod.customFields.bnpl = newValue;
+      }
+    }
+  }
+});
---
+++ src/Resources/app/storefront/src/payment/hipay-hosted-fields/handler-hipay-paypal/handler-hipay-paypal.plugin.js
@@ -0,0 +1,62 @@
+import Plugin from 'src/plugin-system/plugin.class';
+
+/**
+ * Plugin hipay for PayPal
+ */
+export default class HandlerHipayPaypalPlugin extends Plugin {
+    static options = {
+        username: null,
+        password: null,
+        environment: null,
+        merchantPayPalId: null,
+        canPayLater: null,
+        amount: null,
+        currency: null,
+        lang: null,
+        styles: null,
+        idResponse: 'hipay-response',
+    };
+
+    init() {
+        // Remove global payment button
+        let element = document.querySelector('#confirmFormSubmit');
+        if (element) {
+            element.remove();
+        }
+
+        this._hipayInstance = new HiPay({
+            username: this.options.username,
+            password: this.options.password,
+            environment: this.options.environment,
+            lang: this.options.lang
+        });
+
+        const config = {
+            template: 'auto',
+            selector: 'paypal-field',
+            merchantPaypalId: this.options.merchantPayPalId,
+            canPayLater: this.options.canPayLater,
+            paypalButtonStyle: {
+                shape: this.options.styles.shape,
+                height: Number(this.options.styles.height),
+                color: this.options.styles.color,
+                label: this.options.styles.label
+            },
+            request: {
+                amount: this.options.amount,
+                currency: this.options.currency,
+                locale: this.options.locale
+            }
+        };
+
+        this._form = document.querySelector('#' + this.options.idResponse).form;
+
+        this._paypalInstance = this._hipayInstance.create('paypal', config);
+
+        this._paypalInstance.on('paymentAuthorized', (function (data) {
+            const inputResponse = document.querySelector('#' + this.options.idResponse);
+            inputResponse.setAttribute('value', JSON.stringify(data));
+            this._form.submit();
+        }).bind(this));
+    }
+}
