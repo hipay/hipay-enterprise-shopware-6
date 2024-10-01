f1e1a425e2dddadfe55f70d5d79a30c3167789f5
hicham Boummaaiz <hboummaaiz.ext@hipay.com>
2024-10-01 12:46:55 +0200

    Remove Paypal Merchant Id from Administration

--- src/Resources/app/administration/src/settings/hipay-settings-paypal/index.js
+++ src/Resources/app/administration/src/settings/hipay-settings-paypal/index.js
@@ -6,6 +6,7 @@ import template from './hipay-settings-paypal.html.twig';
 
 Shopware.Component.register('hipay-settings-paypal', {
   template,
+  inject: ['systemConfigApiService'],
   props: {
     isLoading: {
       type: Boolean,
@@ -18,7 +19,13 @@ Shopware.Component.register('hipay-settings-paypal', {
   },
   data() {
     return {
-      isPayPalV2: this.paymentMethod.customFields.merchantPayPalId !== "",
+      config: null,
+      environment: null,
+      apiUsername: null,
+      apiPassword: null,
+      authorizationHeader: null,
+      availablePaymentProducts: null,
+      isPayPalV2: null,
       colors: [
         {
           label: this.$tc('hipay.settings.paypal.color.gold'),
@@ -72,17 +79,79 @@ Shopware.Component.register('hipay-settings-paypal', {
         }]
     };
   },
+  mounted() {
+    this.fetchConfig();
+  },
   methods: {
-    updatePayPalStatus(newValue) {
-      this.isPayPalV2 = newValue !== "" ? true : false;
-      this.updateMerchantPayPalId(newValue);
+    fetchConfig() {
+      this.systemConfigApiService.getValues('HiPayPaymentPlugin').then(async (config) => {
+        this.config = config;
+        this.environment = config['HiPayPaymentPlugin.config.environment'];
+        this.setApiCredentials();
+        this.generateAuthorizationHeader();
+        const products = await this.fetchAvailablePaymentProducts();
+        this.processPayPalOptions(products);
+      }).catch(() => {
+        this.isLoading = false;
+      });
     },
-    updateMerchantPayPalId(newValue) {
-      if (this.paymentMethod.customFields === null) {
-        this.paymentMethod.customFields = { ...this.paymentMethod.customFields, merchantPayPalId: newValue };
-      } else {
-        this.paymentMethod.customFields.merchantPayPalId = newValue;
+    setApiCredentials() {
+      const loginKey = this.environment === 'Stage' ? 'publicLoginStage' : 'publicLoginProduction';
+      const passwordKey = this.environment === 'Stage' ? 'publicPasswordStage' : 'publicPasswordProduction';
+      this.apiUsername = this.config[`HiPayPaymentPlugin.config.${loginKey}`];
+      this.apiPassword = this.config[`HiPayPaymentPlugin.config.${passwordKey}`];
+    },
+    generateAuthorizationHeader() {
+      const credentials = `${this.apiUsername}:${this.apiPassword}`;
+      const encodedCredentials = btoa(credentials);
+      this.authorizationHeader = `Basic ${encodedCredentials}`;
+    },
+    async fetchAvailablePaymentProducts() {
+      const baseUrl = this.environment === 'Stage'
+        ? 'https://stage-secure-gateway.hipay-tpp.com/rest/v2/'
+        : 'https://secure-gateway.hipay-tpp.com/rest/v2/';
+
+      const url = new URL(`${baseUrl}available-payment-products.json`);
+      url.searchParams.append('eci', 7);
+      url.searchParams.append('operation', 4);
+      url.searchParams.append('payment_product', 'paypal');
+      url.searchParams.append('with_options', 'true');
+
+      try {
+        const response = await fetch(url, {
+          method: 'GET',
+          headers: {
+            'Authorization': this.authorizationHeader,
+            'Accept': 'application/json'
+          }
+        });
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        this.availablePaymentProducts = await response.json();
+        return this.availablePaymentProducts;
+      } catch (error) {
+        console.error('Error fetching available payment products:', error);
+        return null;
       }
+    },
+    processPayPalOptions(products) {
+      if (products && Array.isArray(products)) {
+        const paypalProduct = products.find(product => product.code === 'paypal');
+        if (paypalProduct && paypalProduct.options) {
+          this.isPayPalV2 = paypalProduct.options.payer_id !== '' &&
+            paypalProduct.options.provider_architecture_version === 'v1';
+        } else {
+          this.resetPayPalStatus();
+        }
+      } else {
+        this.resetPayPalStatus();
+      }
+    },
+    resetPayPalStatus() {
+      this.isPayPalV2 = false;
     },
     updateColor(newValue) {
       if (this.paymentMethod.customFields === null) {
